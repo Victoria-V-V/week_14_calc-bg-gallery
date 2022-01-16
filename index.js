1735e8404543340e8678dd7921310da95ce4efdd
Victoria-V-V <victoriavolcheck@yandex.ru>
2022-01-17 03:53:59 +0700

    Added color change

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-
+//Практическое задание №1
 function add() {
     let number1 = Number(document.getElementById('number1').value);
     let number2 = Number(document.getElementById('number2').value);
@@ -27,3 +26,15 @@ function multiplay(){
     let result = number1 * number2;
     document.getElementById('result').innerHTML = result;
 }
+
+//Практическое задание №2
+function colorWhiteChange(){
+    let colorWhite = document.getElementById('colorWhite');
+    colorWhite.style.backgroundColor = 'gray';
+}
+
+function colorGreyChange(){
+    let colorGrey = document.getElementById('colorGrey');
+    colorGrey.style.backgroundColor = 'white';
+}
+
